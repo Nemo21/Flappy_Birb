e6f1827d9fe21a9ad1ffc24949140cab964d2c6f
Nemo21 <saggubableen9@gmail.com>
2025-02-19 17:40:23 +0530

    Parallax effect for background and ground

--- README.md
+++ README.md
@@ -15,3 +15,21 @@
 - Dont want image to be blurry and upscalled (use nearest neighbour filtering)
 - Set title and get the quit working
 - Get resize working by using push's function to resize
+
+## Lesson 1:
+- We will now implement having a parallax effect
+- What is parallax effect ?
+    - You’re looking out the window of a car or bus while it’s moving. What happens?
+        - The things closest to the window (like a fence or a tree) zoom past super fast.
+        - The things further back (like houses or hills) move more slowly.
+        - The faraway stuff (like mountains or the sky) barely moves at all.
+    - This movement creates a sense of depth even though everything is flat.
+    
+- We will keep track of horizonal scroll position 
+- We will also have a speed of backgrounds to control how fast each layer scrolls
+- The ground will scroll faster to create a parallax effect
+- % very important to sort of "loop" it back to 0 , creating continous scrolling 
+- looping point will be the width of the image to make sure after the % it reaches back to the starting of the image 
+- Make this frame rate independent too to have consistent scrolling speed 
+- Offset will the position shit which will have a left to right scrolling effect instead of moving the image block by block
+- TODO:Reiterate on it again 
--- main.lua
+++ main.lua
@@ -14,8 +14,19 @@ VIRTUAL_WIDTH = 512
 -- Storing it in an object we can draw to the setupScreen
 -- now they are considered "drawable" by love.draw()
 local background = love.graphics.newImage("background.png")
+-- background image and starting scroll location (X axis)
+local backgroundScroll = 0
 local ground = love.graphics.newImage("ground.png")
+-- ground image and starting scroll location (X axis)
+local groundScroll = 0
 
+-- speed at which we should scroll images,each frame by dt
+local BACKGROUND_SCROLL_SPEED = 30
+-- the ground will move twice as fast as the background
+local GROUND_SCROLL_SPEED = 60
+
+-- point at which we should loop our background back to X 0
+local BACKGROUND_LOOPING_POINT = 413
 -- Nothing new called at the start of program execution
 function love.load()
     -- clear and retro 
@@ -43,13 +54,22 @@ function love.keypressed(key)
     end
 end
 
+function love.update(dt)
+    -- scroll background by preset speed * dt, looping back to 0 after the looping point
+    backgroundScroll = (backgroundScroll + BACKGROUND_SCROLL_SPEED * dt) % BACKGROUND_LOOPING_POINT;
+    -- scroll ground by preset speed * dt, looping back to 0 after the screen width passes
+    groundScroll = (groundScroll + GROUND_SCROLL_SPEED * dt) % VIRTUAL_WIDTH
+
+end
 -- this is love's render function which will draw in the push's specified resolution
 
 function love.draw()
     push:start()
-
-    love.graphics.draw(background, 0, 0)
-    love.graphics.draw(ground, 0, VIRTUAL_HEIGHT - 16)
+    -- draw the background at negative looping point having the effect of moving left to right
+    love.graphics.draw(background, -backgroundScroll, 0)
+    -- draw the ground on top of the background, toward the bottom of the screen,
+    -- at its negative looping point
+    love.graphics.draw(ground, -groundScroll, VIRTUAL_HEIGHT - 16)
 
     push:finish()
 end
